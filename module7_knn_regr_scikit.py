1caaef0daf49a0b394ec12a940d05eec8d9decbc
Shitian Wan <shitianw@outlook.com>
2024-03-01 00:47:14 -0800

    MSCS2022 Assignment #7.2 - update based on the feedback from assignment 6.2

--- module7_knn_regr_scikit.py
+++ module7_knn_regr_scikit.py
@@ -16,24 +16,22 @@ def main():
         print("Error: k should <= N")
         return
 
-    train = []
+    train = np.zeros((N,2))
     print("Enter the coordinates for each point (x, y):")
     for i in range(N):
         x = float(input("Enter x coordinate for point {}: ".format(i+1)))
         y = float(input("Enter y coordinate for point {}: ".format(i+1)))
-        train.append([x, y])
+        train[i] = [x, y]
 
-    train = np.array(train)
-
-    X_train = train[:, 0].reshape(-1, 1)
+    x_train = train[:, 0].reshape(-1, 1)
     y_train = train[:, 1]
 
     X_test = np.array([[float(input("Enter the value of for prediction: "))]])
-    y_pred = knn_regression(X_train, y_train, X_test, k)
+    y_pred = knn_regression(x_train, y_train, X_test, k)
 
     print("Predicted value using k-NN Regression:", y_pred[0])
 
-    r2 = r2_score(y_train, knn_regression(X_train, y_train, X_train, k))
+    r2 = r2_score(y_train, knn_regression(x_train, y_train, x_train, k))
     print(f"Coefficient of Determination (R^2): {r2}")
 
 if __name__ == "__main__":
